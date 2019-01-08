1a4c5a6bd68bb41b89dcd62e3d7f5a18a7c49c1a
Arkan <arkanheal@hotmail.fr>
2019-01-08 13:54:41 +0100

    Fixed (now works with indoor HS as well)

--- main.lua
+++ main.lua
@@ -19,17 +19,31 @@ local options = {
 function BetterWype:OnInitialize()
 	LibStub("AceConfig-3.0"):RegisterOptionsTable("BetterWype", options)
 	self.optionFrame = LibStub("AceConfigDialog-3.0"):AddToBlizOptions("BetterWype","BetterWype")
-	self:RegisterChatCommand("bw", "ChatCommand")
 	self:RegisterChatCommand("betterwype", "ChatCommand")
 	BetterWype.message = "Welcome home!"
 end
 
 function BetterWype:OnEnable()
 	self:RegisterEvent("ZONE_CHANGED")
+	self:RegisterEvent("ZONE_CHANGED_INDOORS")
 end
 
 function BetterWype:ZONE_CHANGED()
-	if GetBindLocation() == GetSubZoneText() then
+	local HS_loc = GetBindLocation()
+	HS_loc = HS_loc:lower()
+	local player_Subloc = GetSubZoneText()
+	player_Subloc = player_Subloc:lower()
+	if HS_loc == player_Subloc then
+		self:Print(self.message)
+	end
+end
+
+function BetterWype:ZONE_CHANGED_INDOORS()
+	local HS_loc = GetBindLocation()
+	HS_loc = HS_loc:lower()
+	local player_Subloc = GetSubZoneText()
+	player_Subloc = player_Subloc:lower()
+	if HS_loc == player_Subloc then
 		self:Print(self.message)
 	end
 end
@@ -44,8 +58,8 @@ end
 
 function BetterWype:ChatCommand(input)
 	if not input or input:trim() == "" then
-		InterfaceOptionsFrame_OpenToCategory(self.optionsFrame)
+		InterfaceOptionsFrame_OpenToCategory(self.optionFrame)
 	else
-		LibStub("AceConfigCmd-3.0"):HandleCommand("bw", "BetterWype", input)
+		LibStub("AceConfigCmd-3.0"):HandleCommand(BetterWype, "betterwype", optionFrame, input)
 	end
 end
